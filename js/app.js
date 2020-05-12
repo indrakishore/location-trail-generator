7769761be2e9c75cefeb57f127d0b7cfc08c8cb9
shubham-pal <shubham.mca19.du@gmail.com>
2020-05-12 11:20:25 +0530

    icons

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Initial Map 
-const map = L.map('map-container').setView([28.7041, 77.1025], 2);
+const map = L.map('map').setView([28.7041, 77.1025], 2);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
